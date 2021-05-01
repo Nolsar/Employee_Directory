72ffb20e4b4ae7269e21a471299a2b60e52acd60
Sara Nolan <saraa.ann28@gmail.com>
2021-05-01 14:37:45 -0400

    added components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
-import SearchResultContainer from "./components/SearchResultContainer";
+import React from 'react'
+import Navbar from './components/navBar'
+import Footer from './components/footer'
+import MainContainer from './components/mainContainer'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './components/style.css';
 
 function App() {
-  return <SearchResultContainer />;
+  
+
+  return (
+    <div className="App">
+      <Navbar />
+      <MainContainer />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
